79e468165399bd24aa28faeb51f842fa22a51bef
Huynh Duc Dung <dunghd.it@gmail.com>
2023-12-13 16:33:01 +0800

    feat: introduce new config for simplify error message
    Only show the 1st error from typechecking

--- lua/typecheck/init.lua
+++ lua/typecheck/init.lua
@@ -2,6 +2,7 @@
 local default_config = {
   debug = false,
   mode = 'trouble',
+  only_show_first_error_message = true,
 }
 --- Global configuration for entire plugin, easy to access from anywhere
 _TYPECHECK_GLOBAL_CONFIG = default_config
@@ -11,6 +12,7 @@ local M = {}
 ---@param options (table | nil)
 --       - debug: (boolean | nil) default: false.
 --       - mode: ('trouble' | 'quickfix') default: trouble.
+--       - only_show_first_error_message: (boolean | nil) default: true.
 function M.setup(options)
   _TYPECHECK_GLOBAL_CONFIG =
     vim.tbl_extend('force', _TYPECHECK_GLOBAL_CONFIG, options or default_config)
